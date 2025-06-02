845aba8091efe6dd5723bc8a96be152ded0bfce7
anIRI26 <shtefanesa.iri@gmail.com>
2025-06-02 12:02:18 +0300

    Solution2

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -11,26 +11,29 @@ describe(`Function 'arrayReverse':`, () => {
 
   });
 
-  it(`should return an empty string
+  it(`should return an array with an empty string
     if original array consists of an empty string`, () => {
     expect(arrayReverse([''])).toEqual(['']);
   });
 
   // write more tests here
-  it(`should return array with length
-    = original length`, () => {
+  it(`should return array with same length
+    and string order preserved`, () => {
     expect(arrayReverse(['Iryna', 'Shtef'])).toEqual(['fethS', 'anyrI']);
   });
 
-  it(`should return array with letters and numbers`, () => {
+  it(`should return array of same length preserving order,
+    using letters and numbers`, () => {
     expect(arrayReverse(['Irina26'])).toEqual(['62anirI']);
   });
 
-  it(`should return array with letters and specail symbols`, () => {
+  it(`should return array of same length preserving order,
+    using letters and special characters`, () => {
     expect(arrayReverse(['!Iryna!', 'SHT'])).toEqual(['THS!any', 'rI!']);
   });
 
-  it(`should return array with number and specail symbols`, () => {
+  it(`should return array of same length preserving order,
+    using numbers and special characters`, () => {
     expect(arrayReverse(['26!', '!04!'])).toEqual(['!40', '!!62']);
   });
 });
