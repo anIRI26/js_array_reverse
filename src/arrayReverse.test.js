b3440b81f7466be00e52689cfbf6bd66a386df19
anIRI26 <shtefanesa.iri@gmail.com>
2025-06-02 11:47:09 +0300

    Solution

--- src/arrayReverse.test.js
+++ src/arrayReverse.test.js
@@ -13,8 +13,24 @@ describe(`Function 'arrayReverse':`, () => {
 
   it(`should return an empty string
     if original array consists of an empty string`, () => {
-    expect(arrayReverse(['Mate', 'Academy'])).toEqual(['ymed', 'acAetaM']);
+    expect(arrayReverse([''])).toEqual(['']);
   });
 
   // write more tests here
+  it(`should return array with length
+    = original length`, () => {
+    expect(arrayReverse(['Iryna', 'Shtef'])).toEqual(['fethS', 'anyrI']);
+  });
+
+  it(`should return array with letters and numbers`, () => {
+    expect(arrayReverse(['Irina26'])).toEqual(['62anirI']);
+  });
+
+  it(`should return array with letters and specail symbols`, () => {
+    expect(arrayReverse(['!Iryna!', 'SHT'])).toEqual(['THS!any', 'rI!']);
+  });
+
+  it(`should return array with number and specail symbols`, () => {
+    expect(arrayReverse(['26!', '!04!'])).toEqual(['!40', '!!62']);
+  });
 });
